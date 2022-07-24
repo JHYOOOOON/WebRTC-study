e70c36aabd4b576166762bc2cb11e0d112e7b950
JHYOOOOON <jhyooooooon@gmail.com>
2022-07-24 20:33:56 +0900

    feat: 장치 제거 및 추가 인식

--- script.js
+++ script.js
@@ -3,12 +3,41 @@
     const cameraList = document.querySelector("#cameraList");
     const audioList = document.querySelector("#audioList");
     const speakerList = document.querySelector("#speakerList");
+    const selectedDevices = {
+        camera: "",
+        audio: "",
+        speaker: ""
+    };
     let localStream = null;
 
     const initialSetting = () => {
+        registerChangeEvent();
+        registerDeviceUpdateEvent();
         checkPermissions();
         updateList();
         startStreaming();
+    }
+
+    const registerChangeEvent = () => {
+        cameraList.addEventListener("change", () => {
+            saveDevices("camera", cameraList.value);
+            startStreaming();
+        })
+        audioList.addEventListener("change", () => {
+            saveDevices("audio", audioList.value);
+            startStreaming();
+        })
+        speakerList.addEventListener("change", () => {
+            saveDevices("speaker", speakerList.value);
+            startStreaming();
+        })
+    }
+
+    const registerDeviceUpdateEvent = () => {
+        navigator.mediaDevices.ondevicechange = (event) => {
+            updateList();
+            startStreaming();
+        }
     }
     
     const checkPermissions = () => {
@@ -25,25 +54,40 @@
         const audios = mediaList.filter((media) => media.kind === "audioinput");
         const speakers = mediaList.filter((media) => media.kind === "audioinput");
         
-        viewList(cameraList, cameras);
-        viewList(audioList, audios);
-        viewList(speakerList, speakers);
+        viewList(cameraList, cameras, "camera");
+        viewList(audioList, audios, "audio");
+        viewList(speakerList, speakers, "speaker");
     }
 
-    const viewList = (element, list) => {
-        const listElement = list.map((device) => `<option id=${device.deviceId}>${device.label}</option>`).join('');
+    const viewList = (element, list, name) => {
+        const listElement = list.map((device) => `<option value=${device.deviceId}>${device.label}</option>`).join('');
         element.innerHTML = listElement;
+        
+        if (selectedDevices[name]) {
+            element.value = selectedDevices[name];
+        } else {
+            saveDevices(name, list[0].deviceId);
+        }
+    }
+
+    const saveDevices = (name, value) => {
+        selectedDevices[name] = value;
     }
 
     const startStreaming = async () => {
+        if (!selectedDevices) return;
+        if (localStream) {
+            stopStream(localStream);
+        }
+
         const constraints = {
             video: {
-                deviceId: cameraList.value,
+                deviceId: selectedDevices["camera"],
                 width: 1280,
                 height: 720
             },
             audio: {
-                deviceId: audioList.value,
+                deviceId: selectedDevices["audio"],
             }
         }
         localStream = await navigator.mediaDevices.getUserMedia(constraints);
